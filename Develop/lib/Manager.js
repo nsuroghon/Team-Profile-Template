f8fc040387d34eaa066184480d8ce86b37e5e288
Niv Suroghon <nsuroghon@gmail.com>
2020-12-07 16:26:29 -0500

    removing comment in manager.js

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,13 +1,11 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 
-// NEED 4 THINGS FROM INQUIRER
-
 class Manager extends Employee {
     constructor(name, id, email, officeNumber, ) {
         super(name, id, email)
         this.officeNumber = officeNumber;
     }
-    
+
     getRole() {
         return "Manager";
     }
